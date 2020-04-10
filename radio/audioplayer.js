68f6de1bac8b8fcc7f767c37e920ef5d474350c7
Victor Bross <bross.victor@gmail.com>
2020-04-10 14:42:52 +0200

    basic radio added

---
+++ radio/audioplayer.js
@@ -0,0 +1,7 @@
+$( 'audio' ).audioPlayer(
+{
+	classPrefix: 'player', // default value: 'audioplayer'
+	strPlay: 'Play', // default value: 'Play'
+	strPause: 'Pause', // default value: 'Pause'
+	strVolume: 'Volume', // default value: 'Volume'
+});
